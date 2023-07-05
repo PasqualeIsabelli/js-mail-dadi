2e83ee1033bac100d0851431d6aa248becdc9d32
PasqualeIsabelli <pasquale0395@gmail.com>
2023-07-05 22:12:53 +0200

    fix some details

--- js/second-main.js
+++ js/second-main.js
@@ -1,10 +1,14 @@
+
+// Variabili
 const yourDice = Math.floor((Math.random()*6)+1);
 const pcDice = Math.floor((Math.random()*6)+1);
 
 const runBtn = document.getElementById("run-btn");
 
+// Click Event
 runBtn.addEventListener('click', function () {
 
+  // Condizioni
   if (yourDice > pcDice){
     document.getElementById("message").innerHTML = "Hai Vinto!";
   } else if (yourDice == pcDice) {
@@ -13,6 +17,6 @@ runBtn.addEventListener('click', function () {
     document.getElementById("message").innerHTML = "Hai Perso!";
   }
 
-  document.getElementById("your-numbers").innerHTML = yourDice;
-  document.getElementById("pc-numbers").innerHTML = pcDice;
+  document.getElementById("your-dice").innerHTML = yourDice;
+  document.getElementById("pc-dice").innerHTML = pcDice;
 });
